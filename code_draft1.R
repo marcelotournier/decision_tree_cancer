b8954b5c7a3f19a635fc4ae710867e2a8a6d3841
Marcelo Tournier <dr.tournier@gmail.com>
2019-02-14 13:35:39 -0800

    Add files via upload

--- code_draft1.R
+++ code_draft1.R
@@ -23,10 +23,31 @@ cancer$malignant <- as.numeric(as.factor(cancer$malignant))-1
 # only 2 of 14 values are malignant.  We will fill with the bare.nuclei median value for benign tumors
 #filling missing values using vector operations (more efficient than for loops!)
 cancer[is.na(cancer$bare.nuclei),]$bare.nuclei <- median(cancer[cancer$malignant == 0,]$bare.nuclei, na.rm = T)
-  
+
+#building train-test splits:
+#generating test and train data - Data selected randomly with a 80/20 split
+x <- cancer
+trainIndex  <- sample(1:nrow(x), 0.8 * nrow(x))
+train <- x[trainIndex,]
+test <- x[-trainIndex,]
+ 
 # Modelling Decision Tree:
 mytree <- rpart(malignant ~ tumor.thickness+uniform.cell.size+uniform.cell.shape+margin.adhesion+
-                  epitelial.cell.size+bare.nuclei+bland.chromatin+normal.nucleoli+mitoses+malignant, data = cancer, 
+                  epitelial.cell.size+bare.nuclei+bland.chromatin+normal.nucleoli+mitoses+malignant, data = train, 
                 method = "class") #, control=rpart.control(minsplit=50, cp=0.013))
-# Plotting Decision Tree:
+
+# testing predictions on the model:
+t_pred <- predict(mytree,test,type="class")
+t <- test['malignant']
+
+#building a confusion matrix
+confMat <- table(test$malignant,t_pred)
+
+# getting model accuracy:
+accuracy <- sum(diag(confMat))/sum(confMat)
+print(accuracy) # 0.9285714 -> 92.8%  Not bad!!!
+
+# Plotting our Decision Tree:
 rpart.plot(mytree, type = 1, extra=1)
+
+
